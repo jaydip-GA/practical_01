052ac022e362b21efc8a9643530d7e91150de906
jaydip-GA <jaydip.solanki@green-apex.com>
2021-08-25 17:02:17 +0530

    add space

--- src/Service/service.js
+++ src/Service/service.js
@@ -6,6 +6,7 @@ const { REACT_APP_BASE_URL } = process.env;
 
 
 export const getData = page => {
+
     const URL = `${REACT_APP_BASE_URL}search_by_date?tags=story&page=${page}`;
     return axios.get(URL);
 }
